bded2884a4e547f5680de8af06b89a9ee61d0377
leo108 <leo108@qq.com>
2017-08-25 14:46:22 +0800

    新增部门接口，完善成员接口

---
+++ src/Department/Department.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: leo108
+ * Date: 2017/8/25
+ * Time: 14:15
+ */
+
+namespace Leo108\WorkWechat\Department;
+
+use Leo108\WorkWechat\Core\BaseApi;
+
+class Department extends BaseApi
+{
+    const API_CREATE = 'department/create';
+    const API_UPDATE = 'department/update';
+    const API_DELETE = 'department/delete';
+    const API_LIST = 'department/list';
+
+    public function create($data)
+    {
+        return static::parseJson($this->apiJson(self::API_CREATE, $data));
+    }
+
+    public function update($id, $data)
+    {
+        return static::parseJson($this->apiJson(self::API_UPDATE, array_merge($data, ['id' => $id])));
+    }
+
+    public function delete($id)
+    {
+        return static::parseJson($this->apiJson(self::API_DELETE, ['id' => $id]));
+    }
+
+    public function departmentList($id = null)
+    {
+        $query = [];
+        if (!is_null($id)) {
+            $query['id'] = $id;
+        }
+
+        return static::parseJson($this->apiGet(self::API_LIST, $query));
+    }
+}
--- src/User/User.php
+++ src/User/User.php
@@ -12,10 +12,69 @@ use Leo108\WorkWechat\Core\BaseApi;
 
 class User extends BaseApi
 {
-    const API_GET_USER = 'user/get';
+    const API_GET = 'user/get';
+    const API_CREATE = 'user/create';
+    const API_UPDATE = 'user/update';
+    const API_DELETE = 'user/delete';
+    const API_BATCH_DELETE = 'user/batchdelete';
+    const API_SIMPLE_LIST = 'user/simplelist';
+    const API_LIST = 'user/list';
+    const API_CONVERT_TO_OPENID = 'user/convert_to_openid';
+    const API_AUTH_SUCC = 'user/authsucc';
 
-    public function getUser($userId)
+    public function get($userId)
     {
-        return static::parseJson($this->apiGet(self::API_GET_USER, ['userid' => $userId]));
+        return static::parseJson($this->apiGet(self::API_GET, ['userid' => $userId]));
+    }
+
+    public function create($data)
+    {
+        return static::parseJson($this->apiJson(self::API_CREATE, $data));
+    }
+
+    public function update($userId, $data)
+    {
+        return static::parseJson($this->apiJson(self::API_UPDATE, array_merge($data, ['userid' => $userId])));
+    }
+
+    public function delete($userId)
+    {
+        return static::parseJson($this->apiJson(self::API_DELETE, ['userid' => $userId]));
+    }
+
+    public function batchDelete(array $userIdArr)
+    {
+        return static::parseJson($this->apiJson(self::API_BATCH_DELETE, ['useridlist' => $userIdArr]));
+    }
+
+    public function simpleList($departmentId, $fetchChild = false)
+    {
+        return static::parseJson($this->apiJson(self::API_SIMPLE_LIST, [
+            'department_id' => $departmentId,
+            'fetch_child'   => $fetchChild ? '1' : '0',
+        ]));
+    }
+
+    public function userList($departmentId, $fetchChild = false)
+    {
+        return static::parseJson($this->apiJson(self::API_LIST, [
+            'department_id' => $departmentId,
+            'fetch_child'   => $fetchChild ? '1' : '0',
+        ]));
+    }
+
+    public function convertToOpenId($userId, $agentId = null)
+    {
+        $req = ['userid' => $userId];
+        if (!is_null($agentId)) {
+            $req['agentid'] = $agentId;
+        }
+
+        return static::parseJson($this->apiJson(self::API_CONVERT_TO_OPENID, $req));
+    }
+
+    public function authSucc($userId)
+    {
+        return static::parseJson($this->apiGet(self::API_AUTH_SUCC, ['userid' => $userId]));
     }
 }
