3732532e0347542f933864b4fc6980d296fa0c65
leo108 <leo108@qq.com>
2017-08-26 09:30:51 +0800

    漏掉 accessToken

--- src/Core/ContactAgent.php
+++ src/Core/ContactAgent.php
@@ -11,13 +11,22 @@ namespace Leo108\WorkWechat\Core;
 use Leo108\WorkWechat\Department\Department;
 use Leo108\WorkWechat\User\User;
 
+
+/**
+ * Class ContactAgent
+ * @package Leo108\WorkWechat
+ * @property \Leo108\WorkWechat\Core\AccessToken      $accessToken
+ * @property \Leo108\WorkWechat\User\User             $user
+ * @property \Leo108\WorkWechat\Department\Department $department
+ */
 class ContactAgent extends Agent
 {
     protected function getApiMap()
     {
         return [
-            'user'       => User::class,
-            'department' => Department::class,
+            'accessToken' => AccessToken::class,
+            'user'        => User::class,
+            'department'  => Department::class,
         ];
     }
 }
